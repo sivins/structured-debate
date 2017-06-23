891e40ed4083235d0159075ea4f02870d3d93ba2
sivins <sean.ivins@yahoo.com>
2017-06-22 21:28:45 -0500

    Add project files.

---
+++ StructuredDebate/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(StructuredDebate.Startup))]
+namespace StructuredDebate
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
